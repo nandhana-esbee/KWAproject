69f44e1e8011e989e743b704957dcb28a4e62df1
Nandhana SB <83494871+nandhana-esbee@users.noreply.github.com>
2023-05-14 18:35:40 +0530

    Add files via upload

---
+++ benefits.js
@@ -0,0 +1,10 @@
+
+function show(){
+    var x=document.getElementById("calculate");
+    if(x.style.display=="none"){
+        x.style.display="list-item";
+    }
+    else{
+        x.style.display="none";
+    }
+}
